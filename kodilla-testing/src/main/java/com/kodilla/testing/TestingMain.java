b0064e0561c0b887154bf9c9c5190c6e3b42cb14
Tomasz <tomasz@schroeder.city>
2019-03-12 19:51:22 +0100

    Third Commit

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,8 +1,39 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
 
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if(result.equals("theForumUser")) {
+            System.out.println("Test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        System.out.println("\nTest - pierwszy test jednostkowy");
+
+        Calculator calculator = new Calculator(10, 3);
+
+        int resultOfAdding = calculator.add(10, 3);
+        int resultOfSubstracting = calculator.substract(10,3);
+
+        if(resultOfAdding == 13) {
+            System.out.println("Calculator's adding test OK!");
+        } else {
+            System.out.println("Error!");
+        }
+
+        if(resultOfSubstracting == 7) {
+            System.out.println("Calculator's substracting test OK!");
+        } else {
+            System.out.println("Error!");
+        }
+
     }
 }
