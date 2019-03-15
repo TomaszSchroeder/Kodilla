6c97a573b52bb779d78b8d018f58e82e83196468
Tomasz <tomasz@schroeder.city>
2019-03-15 20:11:23 +0100

    Before creating tests for ShapeCollectorTestSuite

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/shape/ShapeCollector.java
@@ -0,0 +1,22 @@
+package com.kodilla.testing.shape;
+
+public class ShapeCollector {
+
+    public void addFigure(Shape shape) {
+        // it doesn't do anything yet
+    }
+
+    public void removeFigure(Shape shape) {
+        // it doesn't do anything yet
+    }
+
+    public Shape getFigure(int n) {
+      // it doesn't do anything yet
+      return null;
+    }
+
+    public Shape showFigures() {
+        // it doesn't do anything yet
+        return null;
+    }
+}
