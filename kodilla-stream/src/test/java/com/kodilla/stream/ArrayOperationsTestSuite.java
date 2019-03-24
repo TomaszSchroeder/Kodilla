df70de970a71cd5195935a43976bddd7fc6692c7
Tomasz <tomasz@schroeder.city>
2019-03-24 13:14:32 +0100

    Task 6.5

---
+++ kodilla-stream/src/test/java/com/kodilla/stream/ArrayOperationsTestSuite.java
@@ -0,0 +1,30 @@
+package com.kodilla.stream;
+
+import com.kodilla.stream.array.ArrayOperations;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.OptionalDouble;
+
+
+public class ArrayOperationsTestSuite {
+
+    @Test
+    public void testGetAverage() {
+        // Given
+
+
+        int[] numbers = {1, 3, 5, 7, 9, 2, 44, 666};
+
+        // When
+        double output = ArrayOperations.getAverage(numbers);
+        OptionalDouble preExpected = Arrays.stream(numbers).average();
+        double expected = preExpected.getAsDouble();
+
+        // Then
+        Assert.assertEquals(expected, output, 0.01);
+
+
+    }
+}
