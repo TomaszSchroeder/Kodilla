6f07e73631948b73e686cb9057b9425f989468f1
Tomasz <tomasz@schroeder.city>
2019-03-27 22:27:54 +0100

    Task 8.4 first attempt

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FlightRunner.java
@@ -0,0 +1,59 @@
+package com.kodilla.exception.test;
+
+public class FlightRunner {
+
+    public static void main(String[] args) {
+
+        Flight flightOne = new Flight("Berlin", "Hamburg");
+        Flight flightTwo = new Flight("Montreal", "Tokio");
+        Flight flightThree = new Flight("Malaga", "Honolulu");
+        Flight flightFour = new Flight("Madras", "Johannesburg");
+
+        FlightSeeker flightSeeker = new FlightSeeker();
+
+        try {
+            System.out.println("Flight no.1: from Berlin to Hamburg");
+            if (flightSeeker.findFlight(flightOne))
+                System.out.println(flightSeeker.findFlight(flightOne));
+            System.out.println("Flight available, please book your ticket(s)");
+
+        } catch (RouteNotFoundException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            System.out.println("Thank You for choosing our airlines\n");
+        }
+
+        try {
+            System.out.println("Flight no.2: from Montreal to Tokio");
+            if (flightSeeker.findFlight(flightTwo))
+                System.out.println("Flight available, please book your ticket(s)");
+        } catch (RouteNotFoundException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            System.out.println("Thank You for choosing our airlines\n");
+        }
+
+        try {
+            System.out.println("Flight no.3: from Malaga to Honolulu");
+            if (flightSeeker.findFlight(flightThree))
+                System.out.println("Flight available, please book your ticket(s)");
+        } catch (RouteNotFoundException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            System.out.println("Thank You for choosing our airlines\n");
+        }
+
+        try {
+            System.out.println("Flight no.4: from Madras to Johannesburg");
+            if (flightSeeker.findFlight(flightFour))
+                //System.out.println(flightSeeker.findFlight(flightFour));
+                System.out.println("Flight available, please book your ticket(s)");
+        } catch (RouteNotFoundException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            System.out.println("Thank You for choosing our airlines\n");
+        }
+
+
+    }
+}
---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FlightSeeker.java
@@ -0,0 +1,49 @@
+package com.kodilla.exception.test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FlightSeeker {
+
+    public boolean findFlight(Flight flight) throws RouteNotFoundException {
+        Map<String, Boolean> airports = new HashMap<>();
+
+        airports.put("Hamburg", true);
+        airports.put("Malaga", true);
+        airports.put("Reykjavik", true);
+        airports.put("Madras", false);
+        airports.put("Tokio", true);
+        airports.put("Naples", true);
+        airports.put("Johannesburg", false);
+        airports.put("New York", false);
+        airports.put("Berlin", true);
+        airports.put("Athens", false);
+
+        String arrivalAirport = flight.getArrivalAirport();
+        String departureAirport = flight.getDepartureAirport();
+
+        boolean arrivalAirportExists = airports.containsKey(arrivalAirport);
+        boolean departureAirportExists = airports.containsKey(departureAirport);
+
+        boolean available = airports.containsValue(true);
+
+        if (arrivalAirportExists && departureAirportExists) {
+            return airports.get(arrivalAirport) && airports.get(departureAirport);
+
+        } else {
+
+            if (!arrivalAirportExists && !departureAirportExists) {
+                throw new RouteNotFoundException("There are no airports like these!");
+            } else if (!arrivalAirportExists) {
+                throw new RouteNotFoundException("Arrival airport don't exist!");
+            } else if (!departureAirportExists) {
+                throw new RouteNotFoundException("Departure airport don't exist!");
+            } else if ((arrivalAirportExists && !available) || (departureAirportExists && !available)) {
+                throw new RouteNotFoundException("Airport is closed");
+            } else
+
+                throw new RouteNotFoundException("Flight impossible today!");
+
+        }
+    }
+}
